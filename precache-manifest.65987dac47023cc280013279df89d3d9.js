9d717834aa653cb4ee7669f9c8d840045490be2c
Ali Azgar Mithaigar <aliazgar@Alis-MacBook-Pro.local>
2020-08-07 01:58:42 +0530

    Updates

---
+++ precache-manifest.65987dac47023cc280013279df89d3d9.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdd931881a870b78b7b7823bd04e2049",
+    "url": "/dashboard/index.html"
+  },
+  {
+    "revision": "b4ae1b0bb248fd8d5096",
+    "url": "/dashboard/static/css/5.e47cf010.chunk.css"
+  },
+  {
+    "revision": "3658b7e66f89f827e3bc",
+    "url": "/dashboard/static/css/6.e47cf010.chunk.css"
+  },
+  {
+    "revision": "434bfb4a53a9c1b371d6",
+    "url": "/dashboard/static/css/7.e47cf010.chunk.css"
+  },
+  {
+    "revision": "ae68eeefd3751b0409ba",
+    "url": "/dashboard/static/css/8.e47cf010.chunk.css"
+  },
+  {
+    "revision": "e256982ec07fbfbf0592",
+    "url": "/dashboard/static/css/9.e47cf010.chunk.css"
+  },
+  {
+    "revision": "1674db1795c77b55b6d1",
+    "url": "/dashboard/static/css/main.841ce8c2.chunk.css"
+  },
+  {
+    "revision": "814aaecf585129391dd8",
+    "url": "/dashboard/static/js/0.64bfc7aa.chunk.js"
+  },
+  {
+    "revision": "fd7525d544dd9c67d07855cb8778e590",
+    "url": "/dashboard/static/js/0.64bfc7aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e41e78c21deda7dea9f7",
+    "url": "/dashboard/static/js/10.570f6464.chunk.js"
+  },
+  {
+    "revision": "9a90d3439ec07f061a79",
+    "url": "/dashboard/static/js/3.60fd94c6.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/dashboard/static/js/3.60fd94c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36664fd32f626f2cdee1",
+    "url": "/dashboard/static/js/4.56bdd72e.chunk.js"
+  },
+  {
+    "revision": "e7cad5e8c80a4a756b57f883a8b131b2",
+    "url": "/dashboard/static/js/4.56bdd72e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4ae1b0bb248fd8d5096",
+    "url": "/dashboard/static/js/5.5d17f534.chunk.js"
+  },
+  {
+    "revision": "3658b7e66f89f827e3bc",
+    "url": "/dashboard/static/js/6.acde9faa.chunk.js"
+  },
+  {
+    "revision": "434bfb4a53a9c1b371d6",
+    "url": "/dashboard/static/js/7.9f887aa2.chunk.js"
+  },
+  {
+    "revision": "ae68eeefd3751b0409ba",
+    "url": "/dashboard/static/js/8.b6de3052.chunk.js"
+  },
+  {
+    "revision": "e256982ec07fbfbf0592",
+    "url": "/dashboard/static/js/9.87192d98.chunk.js"
+  },
+  {
+    "revision": "1674db1795c77b55b6d1",
+    "url": "/dashboard/static/js/main.c8005d2d.chunk.js"
+  },
+  {
+    "revision": "9d5ae6d81df1e3ca75f5",
+    "url": "/dashboard/static/js/runtime-main.b58cee6b.js"
+  },
+  {
+    "revision": "61b6ff652bc0b59dcb71f310739e58f5",
+    "url": "/dashboard/static/media/WhiteLogo.61b6ff65.svg"
+  },
+  {
+    "revision": "350f203b369e06e3bf6bcc99b96f4dcc",
+    "url": "/dashboard/static/media/notify.350f203b.svg"
+  },
+  {
+    "revision": "c92019fa50a5f82fb50bda7c5bd7f2be",
+    "url": "/dashboard/static/media/refer.c92019fa.svg"
+  },
+  {
+    "revision": "ca52756580e69783a80a2d9ff9fa7451",
+    "url": "/dashboard/static/media/request.ca527565.svg"
+  },
+  {
+    "revision": "392853af3dacb2330ca95eb5e33d5976",
+    "url": "/dashboard/static/media/users.392853af.svg"
+  },
+  {
+    "revision": "6cfd2294e56cb835ac3c74daa478c9d6",
+    "url": "/dashboard/static/media/vijay_dp.6cfd2294.jpg"
+  },
+  {
+    "revision": "1be4b7c8681e2e50541ee4b550b2edd8",
+    "url": "/dashboard/static/media/wave.1be4b7c8.svg"
+  }
+]);
